91f60d66466d250ae2aed4bb174c5a34861150b2
G-cancio <cancio2005.sim@gmail.com>
2026-08-10 19:44:04 -0300

    Passo 10 finalizado

--- dashboard-ford/src/app/components/dashboard/dashboard.ts
+++ dashboard-ford/src/app/components/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -15,6 +15,7 @@ import { Veiculo } from '../../models/veiculo.model';
 export class Dashboard implements OnInit {
   private router = inject(Router);
   private veiculoService = inject(VeiculoService);
+  private cdr = inject(ChangeDetectorRef);
 
   veiculos: Veiculo[] = [];
   veiculoSelecionado: Veiculo | null = null;
@@ -32,6 +33,8 @@ export class Dashboard implements OnInit {
           if (this.veiculos.length > 0) {
             this.veiculoSelecionado = this.veiculos[0];
           }
+
+          this.cdr.detectChanges();
         }
       },
       error: (err) => console.error('Erro ao carregar veículos:', err)
@@ -39,6 +42,14 @@ export class Dashboard implements OnInit {
   }
 
   onVeiculoChange(): void {}
+
+  getFotoVeiculo(nome: string | undefined): string {
+    if (!nome) return 'ford.png';
+    if (nome === 'Bronco Sport') {
+      return 'broncoSport.png';
+    }
+    return `${nome.toLowerCase()}.png`;
+  }
 
   toggleSidebar() {
     const sidebar = document.getElementById('sidebarMenu');
